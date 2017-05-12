653539c00b78424d7e53eefa509965db9b2c9198
Thomas Deffradas <thomas.deffradas@etu.udamail.fr>
2017-05-12 16:46:59 +0200

    Compréhension MVVM

---
+++ src/Projet.Presentation.Forms/Commands/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Projet.Presentation.Forms.Commands
+{
+    public class RelayCommand : ICommand
+    {
+
+        private Action executeInternal;
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+
+        }
+
+        public void Execute(object parameter)
+        {
+            executeInternal();
+        }
+
+
+        public RelayCommand(Action execute)
+        {
+            executeInternal=execute;
+
+     
+
+
+        }
+    }
+}
--- src/Projet.Presentation.Forms/ViewModel/ViewAccueil.cs
+++ src/Projet.Presentation.Forms/ViewModel/ViewAccueil.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using Projet.Presentation.Forms.Commands;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Projet.Presentation.Forms.ViewModel
 {
-    class ViewAccueil
+   public class ViewAccueil : INotifyPropertyChanged
     {
+        private String profilButtonText;
+
+        public ICommand TestCommand
+        {
+            get;
+            set;
+        }
+
+        public String ProfilButtonText
+        {
+            get
+            {
+                return profilButtonText;
+            }
+            set
+            {
+                profilButtonText = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ProfilButtonText"));
+            }
+
+        }
+        public ICommand ChangeTextCommand
+        {
+            get;
+            set;
+
+        }
+
+
+        public ViewAccueil()
+        {
+            ChangeTextCommand = new RelayCommand(() => ProfilButtonText="J'ai change");
+            TestCommand = new RelayCommand(() => this.ToString());
+            ProfilButtonText = "Text";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
